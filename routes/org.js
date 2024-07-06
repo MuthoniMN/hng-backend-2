61718bda9a56d48b3478afc18b28f178130a7c29
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-06 23:46:53 +0300

    refact: using middleware

--- routes/org.js
+++ routes/org.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { getUserOrgs, getOrg, addUserToOrg, addOrg } = require('../controllers/org');
 const orgRouter = express.Router();
 
+orgRouter.all('*', verifyToken)
+
 orgRouter.get('/', getUserOrgs)
 orgRouter.post('/', addOrg)
 orgRouter.get('/:orgID', getOrg)
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,10 @@
-const express = require('express')
+const express = require('express');
+const { getUser } = require('../controllers/user');
+const verifyToken = require('../middleware/auth');
 const userRouter = express.Router();
+
+userRouter.all('*', verifyToken)
+
+userRouter('/:userId', getUser)
 
 module.exports = userRouter;
