ccd22d20d38de40f60b055ccbd9ac04e49514fde
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-06 15:10:04 +0300

    feat: organisation router

---
+++ routes/org.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { getUserOrgs, getOrg, addUserToOrg, addOrg } = require('../controllers/org');
+const orgRouter = express.Router();
+
+orgRouter.get('/', getUserOrgs)
+orgRouter.post('/', addOrg)
+orgRouter.get('/:orgID', getOrg)
+orgRouter.post('/:orgId/users', addUserToOrg)
+
+module.exports = orgRouter;
