57c8917b464fd3928e8745cc9dcacbcbdb7e1601
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-07 07:33:26 +0300

    bug: splitting token + better error responses

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,24 @@ const jwt = require('jsonwebtoken');
 const { validateToken } = require('../utils/jwt')
 
 function verifyToken(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Access denied' });
+    const payload = req.header('Authorization');
+    const [str, token] = payload.split(" ")
+
+    if (!token) return res.status(401).json({ 
+        status: "Bad Request",
+        message: 'Access denied',
+        code: 401
+    });
     try {
-        validateToken(token)
+        const decoded = validateToken(token)
         req.userId = decoded.userId;
         next();
     } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
+        res.status(401).json({ 
+            status: "Bad Request",
+            message: 'Invalid token',
+            code: 401
+        });
     }
  };
 
