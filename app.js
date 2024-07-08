844e0816a43c149ff8da34143ac155af61ef9a1c
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-08 15:38:54 +0000

    Bug: adding middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
 const orgRouter = require('./routes/org')
-const cors = require('cors')
+const verifyToken = require('./middleware/auth')
 const app = express()
 
 require('dotenv').config({ path: './.env' })
@@ -10,6 +10,7 @@ const port = process.env.PORT
 
 app.use(express.json())
 
+app.use('/api/*', verifyToken)
 // routers
 app.use('/auth', authRouter)
 app.use('/api/user', userRouter)
