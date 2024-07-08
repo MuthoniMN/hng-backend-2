a6b3e547693c1cda0ca7dea738a5fa09b03c1a80
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-08 15:51:41 +0000

    Refact: returning data for current user or users in their organisations

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,30 @@ const prisma = require("../client")
 
 const getUser = async (req, res) => {
     const { userId } = req.params
+    const currentUser = req.userId;
     try {
         const user = await prisma.user.findUniqueOrThrow({
             where: {
                 userId: userId
-            }
+            },
+                include: {
+                    organisations: true
+                }
         })
 
+        if(userId !== currentUser){
+            const current = await prisma.user.findUnique({
+                where: {
+                    userId: currentUser
+                },
+                include: {
+                    organisations: true
+                }
+            })
+            if(!current.organisations.filter(org => user.organisations.include(org.orgId))){
+                throw new Error("You are not allowed to view this user!");
+            }}
+        
         return res.status(200).json({
             status: "success",
             message: "User successfully found!",
