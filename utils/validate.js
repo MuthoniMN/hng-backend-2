8d592e814a6b1461ca46aa8ff1201497602395e9
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-12 11:49:15 +0300

    removing extra validation

--- utils/validate.js
+++ utils/validate.js
@@ -3,38 +3,24 @@ const { z } = require('zod')
 const User = z.object({
     firstName: z.string({
         required_error: "First name is required"
-    }).min(3, {
-        message: "Please enter a valid first name"
     }),
     lastName: z.string({
         required_error: "Last name is required"
-    }).min(3, {
-        message: "Please enter a valid last name"
     }),
     password: z.string({
         required_error: "Password is required"
-    }).min(6, {
-        message: "Password is too short"
     }),
     phone: z.string({
         required_error: "Phone number is required"
-    }).min(10, {
-        message: "Invalid phone number"
-    }).max(14, {
-        message: "Invalid phone number"
     }),
     email: z.string({
         required_error: "Email is required"
-    }).email({
-        message: "Invalid email address"
     })
 })
 
 const LoginUser = z.object({
     email: z.string({
         required_error: "Email is required"
-    }).email({
-        message: "Invalid email address"
     }),
     password: z.string({
         required_error: "Password is required"
